6ca7078105ef9c32146c94f21c9d3c33816db600
骆江尚 <qq1100126@126.com>
2019-09-10 00:05:17 +0800

    Update

--- 人机猜拳-30行.py
+++ 人机猜拳-30行.py
@@ -1,9 +1,6 @@
 import random
 
 
-def win_or_lose(ai, user):
-    return '平' if ai == user else '赢' if ai == (user + 1) % 3 else '输'
-
 def main():
     mora = ['石头', '剪刀', '布']
     ai = random.randint(1, 3)
@@ -12,19 +9,18 @@ def main():
     # 验证输入是否有效
     if user in mora + ['1', '2', '3']:
         user = int(user) if user.isdigit() else mora.index(user) + 1
-        # result = win_or_lose(ai, user)
-        # win_or_lose = lambda ai, user:'平' if ai == user else '赢' if ai == (user + 1) % 3 else '输'
+
+        # 判断输赢
+        win_or_lose = lambda ai, user: '平局' if ai == user else '你赢了' if ai == (user + 1) % 3 else '你输了'
         result = win_or_lose(ai, user)
 
         # 打印结果
-        print(f'你{result}了' if result != '平' else '平局', f'电脑出的拳是：{mora[ai - 1]}\n')
+        print(f'{mora[user - 1]} VS {mora[ai - 1]}\n{result}\n')
 
     else:
         print('你输入有误哦~\n')
 
+print("猜拳小游戏规则：\n  请输入'石头'、'剪刀'、'布'或者'1'、'2'、'3'来选择您要出的拳。\n")
 
-if __name__ == '__main__':
-    print("猜拳小游戏规则：\n  请输入'石头'、'剪刀'、'布'或者'1'、'2'、'3'来选择您要出的拳。\n")
-
-    while True:
-        main()
+while True:
+    main()
